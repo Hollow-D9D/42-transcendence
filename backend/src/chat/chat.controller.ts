5082741ce22c3ca24df6377d8d3080d0ec0de8f5
Arman Mamian <armanmamian@gmail.com>
2023-05-14 15:04:31 +0400

    Add channel membership/privileges mechanics

--- backend/src/chat/chat.controller.ts
+++ backend/src/chat/chat.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Headers, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Headers, UseGuards } from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { getPayload } from 'src/auth/utils';
 import { AuthGuard } from 'src/auth/auth.guard';
@@ -8,27 +8,31 @@ import { ChannelMode, isValidChannelMode } from 'src/typeorm/channelmode.enum';
 export class ChatController {
   constructor(private readonly chatService: ChatService) {}
 
-  // FIVE KEY FIELDS VALIDATED (in parens -- whether mandatory for chats or
-  // channels):
-  // - `login` (chat/channel)
-  // - `target` (chat)
-  // - `mode` (channel)
-  // - `name` (channel)
-  // - `password` (channel)
-  //
-  // VALIDATIONS PERFORMED:
-  // 1. for any input field that is about to be used the first time, an
-  //    appropriate type check is performed
-  // 2. for channels:
-  //   - a user with login `login` is actually present in the DB
-  //   - `mode` is one of the actual modes (PUBLIC/PRIVATE/PROTECTED)
-  //   - there is no channel with the name `name` yet
-  //   - `password` is valid for `PROTECTED` (see `isValidPassword` for more)
-  // 3. for chats:
-  //   - users with login `login` and `target` are actually present in the DB
-  //   - `mode` is null or its value is invalid
-  //   - `login` isn't banned by `target`
-  @Get()
+  /**
+   * CREATE A CHAT/CHANNEL
+   *
+   * FIVE KEY FIELDS VALIDATED (in parens -- whether mandatory for chats or
+   * channels):
+   * - `login` (chat/channel)
+   * - `target` (chat)
+   * - `mode` (channel)
+   * - `name` (channel)
+   * - `password` (channel)
+   *
+   * VALIDATIONS PERFORMED:
+   * 1. for any input field that is about to be used the first time, an
+   *    appropriate type check is performed
+   * 2. for channels:
+   *   - a user with login `login` is actually present in the DB
+   *   - `mode` is one of the actual modes (PUBLIC/PRIVATE/PROTECTED)
+   *   - there is no channel with the name `name` yet
+   *   - `password` is valid for `PROTECTED` (see `isValidPassword` for more)
+   * 3. for chats:
+   *   - users with login `login` and `target` are actually present in the DB
+   *   - `mode` is null or its value is invalid
+   *   - `login` isn't banned by `target`
+   */
+  @Post('create')
   @UseGuards(AuthGuard)
   async create(@Headers() headers) {
     try {
@@ -61,11 +65,11 @@ export class ChatController {
             };
         }
         // retreive user(s)
-        const users = await this.chatService.getUsers(logins);
+        const users = await this.chatService.users(logins);
         // and validate `login`, `target`, `name` and `password` whenever necessary
         if (!mode) {
           // chat, not a channel
-          if (users.length != 2) {
+          if (users.length !== 2) {
             // both `login` and `target` are expected to yield valid users
             // if not, the request in invalid
             return {
@@ -100,9 +104,9 @@ export class ChatController {
             };
           }
           // check that a chat with this name hasn't already been created
-          const chatWithName = await this.chatService.getName(payload.name);
-          if (chatWithName) {
-            // chatWithName !== undefined
+          const channelWithName = await this.chatService.channel(payload.name);
+          if (channelWithName) {
+            // channelWithName !== undefined
             return {
               error: new Error(
                 'A channel with this name has already been created!',
@@ -128,6 +132,258 @@ export class ChatController {
           error: new Error('Invalid input'),
           body: null,
         };
+      }
+    } catch (error) {
+      return { error, body: null };
+    }
+  }
+
+  /**
+   * LEAVE A CHANNEL
+   *
+   * `login` who wants to leave
+   * `name` what channel to leave
+   *
+   * - check that the channel exists
+   * - check that login is a channel member
+   * - proceed to business logic (service.ts)
+   */
+  @Post('leaveChannel')
+  @UseGuards(AuthGuard)
+  async leaveChannel(@Headers() headers) {
+    try {
+      const payload = getPayload(headers);
+      if (payload) {
+        if (typeof payload.login !== 'string') {
+          // shouldn't get here
+          return {
+            error: new Error('Invalid user info!'),
+            body: null,
+          };
+        }
+        if (typeof payload.name !== 'string') {
+          // doesn't have a valid channel `name` specified
+          return {
+            error: new Error('No valid channel name specified!'),
+            body: null,
+          };
+        }
+        const channelWithName = await this.chatService.channel(payload.name);
+        if (!channelWithName) {
+          // no channel with this name
+          return {
+            error: new Error('No channel with this name!'),
+            body: null,
+          };
+        }
+        if (
+          !(await this.chatService.isChannelMember(payload.name, payload.login))
+        ) {
+          // isn't a member of the channel
+          return {
+            error: new Error('You are not a member of this channel!'),
+            body: null,
+          };
+        }
+        this.chatService.leaveChannel(payload.login, payload.name);
+      }
+    } catch (error) {
+      return { error, body: null };
+    }
+  }
+
+  /**
+   * JOIN A CHANNEL
+   *
+   * `login` who's trying to join the channel
+   * `password` of the channel
+   * `name` the target channel
+   *
+   * - check that the channel exists
+   * - check that login exists
+   * - check that password is valid or an empty string
+   * - proceed to business logic (service.ts)
+   */
+  @Post('joinChannel')
+  @UseGuards(AuthGuard)
+  async joinChannel(@Headers() headers) {
+    try {
+      const payload = getPayload(headers);
+      if (payload) {
+        if (typeof payload.login !== 'string') {
+          // shouldn't get here
+          return {
+            error: new Error('Invalid user info!'),
+            body: null,
+          };
+        }
+        if (typeof payload.name !== 'string') {
+          // doesn't have a valid channel `name` specified
+          return {
+            error: new Error('No valid channel name specified!'),
+            body: null,
+          };
+        }
+        const channelWithName = await this.chatService.channel(payload.name);
+        if (!channelWithName) {
+          // no channel with this name
+          return {
+            error: new Error('No channel with this name!'),
+            body: null,
+          };
+        }
+        const userWithLogin = await this.chatService.users([payload.login]);
+        if (userWithLogin.length !== 1) {
+          // no user with the username login
+          return {
+            error: new Error('No user with username login!'),
+            body: null,
+          };
+        }
+        if (
+          typeof payload.password !== 'string' ||
+          (!this.chatService.isValidPassword(payload.password) &&
+            payload.password !== '')
+        ) {
+          // doesn't have a valid/empty-string `password` specified
+          return {
+            error: new Error('No valid password specified!'),
+            body: null,
+          };
+        }
+        this.chatService.joinChannel(
+          payload.login,
+          payload.name,
+          payload.password,
+        );
+      }
+    } catch (error) {
+      return { error, body: null };
+    }
+  }
+
+  /**
+   * GRANT ADMIN PRIVILEGES TO A CHANNEL MEMBER
+   *
+   * `login` who's trying to grant admin privileges
+   * `target` the to-be admin
+   * `name` the target channel
+   *
+   * - check that the channel exists
+   * - check that login and target exist
+   * - proceed to business logic (service.ts)
+   */
+  @Post('grantAdmin')
+  @UseGuards(AuthGuard)
+  async grantAdmin(@Headers() headers) {
+    try {
+      const payload = getPayload(headers);
+      if (payload) {
+        if (typeof payload.login !== 'string') {
+          // shouldn't get here
+          return {
+            error: new Error('Invalid user info!'),
+            body: null,
+          };
+        }
+        if (
+          typeof payload.name !== 'string' ||
+          typeof payload.target !== 'string'
+        ) {
+          // doesn't have a valid channel `name`/`target` user specified
+          return {
+            error: new Error('No valid channel name/target user specified!'),
+            body: null,
+          };
+        }
+        const channelWithName = await this.chatService.channel(payload.name);
+        if (!channelWithName) {
+          // no channel with this name
+          return {
+            error: new Error('No channel with this name!'),
+            body: null,
+          };
+        }
+        const userWithLogin = await this.chatService.users([
+          payload.login,
+          payload.target,
+        ]);
+        if (userWithLogin.length !== 2) {
+          // no user with the username login
+          return {
+            error: new Error('No user with username login/target!'),
+            body: null,
+          };
+        }
+        this.chatService.grantAdmin(
+          payload.login,
+          payload.target,
+          payload.name,
+        );
+      }
+    } catch (error) {
+      return { error, body: null };
+    }
+  }
+
+  /**
+   * REVOKE ADMIN PRIVILEGES FROM A CHANNEL MEMBER
+   *
+   * `login` who's trying to remove admin privileges
+   * `target` who's admin privileges are about to get revoked
+   * `name` the target channel
+   *
+   * - check that the channel exists
+   * - check that login and target exist
+   * - proceed to business logic (service.ts)
+   */
+  @Post('revokeAdmin')
+  @UseGuards(AuthGuard)
+  async revokeAdmin(@Headers() headers) {
+    try {
+      const payload = getPayload(headers);
+      if (payload) {
+        if (typeof payload.login !== 'string') {
+          // shouldn't get here
+          return {
+            error: new Error('Invalid user info!'),
+            body: null,
+          };
+        }
+        if (
+          typeof payload.name !== 'string' ||
+          typeof payload.target !== 'string'
+        ) {
+          // doesn't have a valid channel `name` specified
+          return {
+            error: new Error('No valid channel name/target user specified!'),
+            body: null,
+          };
+        }
+        const channelWithName = await this.chatService.channel(payload.name);
+        if (!channelWithName) {
+          // no channel with this name
+          return {
+            error: new Error('No channel with this name!'),
+            body: null,
+          };
+        }
+        const userWithLogin = await this.chatService.users([
+          payload.login,
+          payload.target,
+        ]);
+        if (userWithLogin.length !== 2) {
+          // no user with the username login
+          return {
+            error: new Error('No user with username login/target!'),
+            body: null,
+          };
+        }
+        this.chatService.revokeAdmin(
+          payload.login,
+          payload.target,
+          payload.name,
+        );
       }
     } catch (error) {
       return { error, body: null };
--- backend/src/chat/chat.service.ts
+++ backend/src/chat/chat.service.ts
@@ -37,7 +37,7 @@ export class ChatService {
       entityLike.group = true;
       entityLike.mode = mode;
       entityLike.name = name;
-      if (mode == ChannelMode.PROTECTED) entityLike.password = password; // TODO: replace with the hashed one
+      if (mode === ChannelMode.PROTECTED) entityLike.password = password; // TODO: replace with the hashed one
       entityLike.owner = users[0]; // the first one is `login` from request
       // TODO: check that the fail condition works properly
     }
@@ -46,7 +46,120 @@ export class ChatService {
     await this.chatRepo.save(newOne);
   }
 
-  async getUsers(logins: string[]): Promise<User[]> {
+  /**
+   * - remove login
+   * - check that there are no users left in the channel OR login is the channel owner ? delete channel
+   *
+   * @param login of the to-leave channel member
+   * @param name of the channel
+   * @returns nothing
+   */
+  async leaveChannel(login: string, name: string) {
+    var channel = await this.channel(name);
+    if (!channel) return;
+    const userToRemove = await channel.members.find(
+      (user) => user.login === login,
+    );
+    if (!userToRemove) return;
+    channel.members = channel.members.filter((user) => user.login !== login);
+    await this.chatRepo.save(channel);
+
+    if (channel.members.length === 0 || channel.owner.login === login) {
+      await this.chatRepo.remove(channel);
+    }
+  }
+
+  /**
+   * - the channel isn't private:
+   *  - check that login isn't a member of the channel and isn't banned from there ?
+   *    - the channel is protected:
+   *      - check that password is correct ? add login to the channel
+   *    - else (the channel is open) ? add login to the channel
+   *
+   * @param login who's trying to join the channel
+   * @param name of the channel
+   * @param password of the channel
+   */
+  async joinChannel(login: string, name: string, password: string) {
+    var channel = await this.channel(name);
+    if (channel.mode != ChannelMode.PRIVATE) {
+      const isChannelMember = channel.members.some(
+        (user) => user.login === login,
+      );
+      const isBannedFromChannel = channel.blocked.some(
+        (user) => user.login === login,
+      );
+      if (!isChannelMember && !isBannedFromChannel) {
+        // TODO: replace with the hashed one
+        if (
+          (channel.mode === ChannelMode.PROTECTED &&
+            channel.password === password) ||
+          channel.mode === ChannelMode.PUBLIC
+        ) {
+          const user = await this.userRepo.findOne({
+            where: { login: login },
+          });
+          channel.members.push(user);
+          await this.chatRepo.save(channel);
+        }
+      }
+    }
+  }
+
+  /**
+   * - check that login is the channel owner
+   * - check that target is a member of the channel
+   * - check that target isn't an admin yet ? add target to the channel admins
+   *
+   * @param login who's trying to grant admin privileges
+   * @param target the to-be admin
+   * @param name of the channel
+   */
+  async grantAdmin(login: string, target: string, name: string) {
+    var channel = await this.channel(name);
+    if (channel.owner.login === login) {
+      const isChannelMember = channel.members.some(
+        (user) => user.login === target,
+      );
+      if (isChannelMember) {
+        const isChannelAdmin = channel.admins.some(
+          (user) => user.login === target,
+        );
+        if (!isChannelAdmin) {
+          const user = await this.userRepo.findOne({
+            where: { login: target },
+          });
+          if (user) {
+            channel.admins.push(user);
+            await this.chatRepo.save(channel);
+          }
+        }
+      }
+    }
+  }
+
+  /**
+   * - check that login is the channel owner
+   * - check that target is a member of the channel with admin privileges ? remove target from the channel admins
+   *
+   * @param login
+   * @param target
+   * @param name
+   */
+  async revokeAdmin(login: string, target: string, name: string) {
+    var channel = await this.channel(name);
+    if (channel.owner.login === login) {
+      const isChannelAdmin = channel.admins.some(
+        (user) => user.login === target,
+      );
+      if (isChannelAdmin) {
+        channel.admins = channel.admins.filter((user) => user.login !== target);
+        await this.chatRepo.save(channel);
+      }
+    }
+  }
+
+  async users(logins: string[]): Promise<User[]> {
     return await this.userRepo.find({ where: { login: In(logins) } });
   }
 
@@ -60,18 +173,47 @@ export class ChatService {
     return blockerFound.blocked_users.some((user) => user.login === blocked);
   }
 
-  async getName(name: string): Promise<Chat> {
+  /**
+   * @param name of the channel to return
+   * @returns Chat entity if found, null otherwise
+   */
+  async channel(name: string): Promise<Chat> {
     return await this.chatRepo.findOne({ where: { name: name } });
   }
 
-  // VALIDATIONS PERFORMED:
-  // - 8-30 chars
-  // - at least one uppercase letter
-  // - at least one lowercase letter
-  // - at least one number
-  // - doesn't have anything apart from upper/lower/number/special chars
-  //   the idea is that only printable non-space ascii characters are allowed
-  //   but I may have missed something
+  /**
+   * check if a channel exists and return its type if it does
+   *
+   * @param name of the channel to be checked
+   * @returns a valid ChannelMode if a channel with this name exists, null otherwise
+   */
+  async channelType(name: string): Promise<ChannelMode> {
+    const channel = await this.channel(name);
+    if (channel) {
+      return channel.mode;
+    }
+    return null;
+  }
+
+  /**
+   * @param name of the channel
+   * @param login of the user to be checked
+   */
+  async isChannelMember(name: string, login: string): Promise<boolean> {
+    const channel = await this.channel(name);
+    return channel.members.some((user) => user.login === login);
+  }
+
+  /**
+   * VALIDATIONS PERFORMED:
+   * - 8-30 chars
+   * - at least one uppercase letter
+   * - at least one lowercase letter
+   * - at least one number
+   * - doesn't have anything apart from upper/lower/number/special chars
+   *   the idea is that only printable non-space ascii characters are allowed
+   *   but I may have missed something
+   */
   isValidPassword(password: string): boolean {
     // 8-30 chars
     if (password.length < 8 || password.length > 30) {
